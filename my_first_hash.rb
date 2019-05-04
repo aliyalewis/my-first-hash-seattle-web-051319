c8c9605f0f1b10e19a24ebac1406633b27b9d4df
Aliya Lewis <aliyalewis5@gmail.com>
2019-05-04 01:01:30 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,7 +15,6 @@ def retrieval
                  }
                  shipping_manifest["oil paintings"]
 end
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 def adding
   shipping_manifest = {
@@ -23,6 +22,7 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
+                 shipping_manifest["muskets"] = 2
   # add 2 muskets to the shipping_manifest hash below
 
 
